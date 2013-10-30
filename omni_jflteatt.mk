9999c65a0f6ff76e7809b1c6e7d5fcaa2f219578
Jacob <jacob.whatley91@gmail.com>
2013-10-30 02:29:34 -0400

    remove whitespace

--- omni_jflteatt.mk
+++ omni_jflteatt.mk
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
- 
 # Grab needed APNs
 $(call inherit-product, vendor/omni/config/gsm.mk)
 
